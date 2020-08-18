571b5d751d1e01cbf664a76ea7037d384f38bbe9
Aline Martineli De Araujo Navarro <aline.navarro@totvs.com.br>
2020-08-18 20:04:46 -0300

    Criação de classe (modelo) Negociação

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,8 +1,27 @@
 class Negociacao {
 
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, qtd, valor) {
+
+        this._data = new Date(data.getTime());
+        this._qtd = qtd;
+        this._valor = valor;
+        Object.freeze(this);
     }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get qtd() {
+        return this._qtd;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._qtd * this._valor;
+    }
+
 }
