ebf00296cd7dec90e5b15c940a9c4cecf0576e5f
Aline Martineli De Araujo Navarro <aline.navarro@totvs.com.br>
2020-08-20 15:45:53 -0300

    Criação de classe DateHelper, uso de regex e template string.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -14,22 +14,14 @@ constructor() {
 
         //let data = new Date(this._inputData.value.replace(/-/g, ','));
         //let data = new Date(this._inputData.value.split('-');
-        
-        // testando data com paradigma funcional:
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-            
-        );
 
         let negociacao = new Negociacao(
-            data,
+            DateHelper.txtParaData(this._inputData.value),
             this._inputQtd.value,
             this._inputValor.value
         );
 
-        console.log(negociacao);
+        console.log(DateHelper.dataParaTxt(negociacao.data));
     }
 
 }
---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,17 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTxt(data) {
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+    }
+
+    static txtParaData(txt) {
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(txt)) throw new Error('Deve estar no formato aaaa-mm-dd');
+        
+        return new Date(...txt.split('-').map((item, indice) => item - indice % 2));
+    }
+}
